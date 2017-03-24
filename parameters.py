cac2e37ce532f60f31fee4eb620e2d3e3483c0af
nicolascasajus <nicolas.casajus@gmail.com>
2017-03-23 20:02:00 -0400

    Add two files and some improvements

--- parameters.py
+++ parameters.py
@@ -1,33 +1,63 @@
 '''
-xxxxxx
+This file contains all parameters the user needs to set to run the Python program
+foxmask.py.
 '''
 
 
 
-# Location of the folder containing photos to analyze
-imagesDir = '/home/vagrant/Desktop/Images/Fox001_2008_Cam2/'
+# Location of the folder containing photos to analyze (do not forget the final /)
+# Can be one folder or a list of folders (but no embedded folders)
+
+imagesDir = ['/media/sf_vagrant/reconyx/Fox024-2008-Cam1-test/',
+	     '/media/sf_vagrant/reconyx/Fox024-2008-Cam2-test/']
 
 
 
-# Location of the folder to store results
-outputDir = '/home/vagrant/Desktop/'
+# Location of the folder to store results (do not forget the final /)
+# A subfolder Results/ will be created
+
+outputDir = '/media/sf_vagrant/reconyx/'
 
 
 
-# Name of the CSV file generated
-ouname = 'Fox001-2008-Cam2'
+# Name of the CSV file(s) generated
+# One file will be created for each input folder (and for each value of minsize, see below)
+
+ouname = ['Fox024-2008-Cam1', 'Fox024-2008-Cam2']
 
 
 
-# Maximum time gap between the consecutive images (to build sequence)
+# Maximum time gap (in seconds) between two consecutive images (to build sequence)
+
 maxgap = 5
 
 
 
-# Deleting masks (0: no OR 1: yes)
+# Deleting masks (0: no OR 1: yes [recommended])
+
 rmmasks = 1
 
 
 
 # Minimum size of detected objects
-minsize = 150
+# Can be one single value or a list of values (to perform sensitivity analyses)
+
+minsize = [5,   10,   15,   20,   25,   30,   35,   40,   45,   50,   55,
+         60,   65,   70,   75,   80,   85,   90,   95,  100,  105,  110,
+        115,  120,  125,  130,  135,  140,  145,  150,  155,  160,  165,
+        170,  175,  180,  185,  190,  195,  200,  205,  210,  215,  220,
+        225,  230,  235,  240,  245,  250,  255,  260,  265,  270,  275,
+        280,  285,  290,  295,  300,  305,  310,  315,  320,  325,  330,
+        335,  340,  345,  350,  355,  360,  365,  370,  375,  380,  385,
+        390,  395,  400,  405,  410,  415,  420,  425,  430,  435,  440,
+        445,  450,  455,  460,  465,  470,  475,  480,  485,  490,  495,
+        500,  505,  510,  515,  520,  525,  530,  535,  540,  545,  550,
+        555,  560,  565,  570,  575,  580,  585,  590,  595,  600,  605,
+        610,  615,  620,  625,  630,  635,  640,  645,  650,  655,  660,
+        665,  670,  675,  680,  685,  690,  695,  700,  705,  710,  715,
+        720,  725,  730,  735,  740,  745,  750,  755,  760,  765,  770,
+        775,  780,  785,  790,  795,  800,  805,  810,  815,  820,  825,
+        830,  835,  840,  845,  850,  855,  860,  865,  870,  875,  880,
+        885,  890,  895,  900,  905,  910,  915,  920,  925,  930,  935,
+        940,  945,  950,  955,  960,  965,  970,  975,  980,  985,  990,
+        995, 1000]
